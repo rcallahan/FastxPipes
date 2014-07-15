3c4518b8ec8827b3ea658e81622797c4ebb8da34
rcallahan <rcallahan@eurekagenomics.com>
2014-07-15 17:40:33 -0500

    no more attoparsec

--- FastxPipe.hs
+++ FastxPipe.hs
@@ -2,9 +2,13 @@ module FastxPipe where
 
 import Data.Attoparsec.Char8
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
+import Data.ByteString (ByteString)
 import Blaze.ByteString.Builder
 import Control.Monad
 import Control.Applicative
+import Control.Exception
+import System.IO
 import Data.Monoid
 import Pipes
 import Pipes.Internal
@@ -15,15 +19,32 @@ import Pipes.Attoparsec hiding (parse)
 takeTill' c = takeTill (== c) <* char c
 skipTill c = skipWhile (/= c) *> char c *> pure ()
 
-parseFastq :: Parser (B.ByteString, B.ByteString, B.ByteString)
-parseFastq = (,,) <$> (char '@' *> takeTill' '\n')
-    <*> (takeTill' '\n' <* skipTill '\n')
-    <*> (takeTill' '\n')
-
-parseFasta :: Parser (B.ByteString, B.ByteString)
+parseFasta :: Parser (ByteString, ByteString)
 parseFasta = (,) <$> (char '>' *> takeTill' '\n') <*> multiline where
     multiline = toByteString . mconcat <$> (many1 $ fromByteString <$> do
         Just c <- peekChar
         if c == '>' then fail "next" else takeTill' '\n')
 
-hFastqProd h = parseMany parseFastq $ Pb.fromHandle h
+hFastqProd :: Handle -> Producer (ByteString, ByteString, ByteString) IO ()
+hFastqProd h = go where
+    go = do
+        let getline = B.hGetLine h
+            doread = do
+                line1 <- getline
+                head <- case B8.uncons line1 of
+                    Just ('@', rest) -> return rest
+                    _ -> ioError $ userError "bad format"
+                sq <- getline
+                blank <- getline
+                case B8.uncons blank of
+                    Just ('+', _) -> return ()
+                    _ -> ioError $ userError "bad format"
+                qual <- getline
+                when (B.length qual /= B.length sq) (ioError $ userError "bad format")
+                return $ Just (head, sq, qual)
+            catcher :: IOException -> IO (Maybe (ByteString, ByteString, ByteString))
+            catcher _ = return Nothing
+        mbread <- liftIO $ catch doread catcher
+        case mbread of
+            Just j -> yield j >> go
+            Nothing -> return ()
